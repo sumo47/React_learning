ddf1b2ff01e8af51ad24a5826e2488bfc29d396d
Sumit <sumokhan76342@gmail.com>
2024-10-31 23:00:44 +0530

    props drilling , lifting state

--- src/components/instaMart.js
+++ src/components/instaMart.js
@@ -5,15 +5,20 @@ const Section = ({ isVisible, title, paragraph, SetIsVisible }) => {
     return (
         <div className='border-black border p-2 m-2'>
             <h2 className='font-bold text-2xl m-2'>{title}</h2>
-            {!isVisible ? <button
-                className="cursor-pointer underline"
-                onClick={() => {
-                    SetIsVisible(false)
-                }}>show</button> : <button
-                    className="cursor-pointer underline"
-                    onClick={() => {
-                        SetIsVisible(true)
-                    }}>Hide</button>}
+            {
+                isVisible ?
+                    <button
+                        className="cursor-pointer underline"
+                        onClick={() => {
+                            SetIsVisible(false)
+                        }}>Hide</button>
+                    :
+                    <button
+                        className="cursor-pointer underline"
+                        onClick={() => {
+                            SetIsVisible(true)
+                        }}>Show</button>
+            }
             {isVisible && <p>{paragraph}</p>}
         </div>
     )
@@ -21,11 +26,7 @@ const Section = ({ isVisible, title, paragraph, SetIsVisible }) => {
 
 const instaMart = () => {
 
-    const [sectionConfig, setSectionConfig] = useState({
-        aboutInstaMart: false,
-        teamInstaMart: true,
-        careerInstaMart: false,
-    })
+    const [visibleSection, setIsVisibleSection] = useState("team")
 
     return (
         <div>
@@ -34,40 +35,22 @@ const instaMart = () => {
             <Section
                 title="About InstaMart"
                 paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti atque ipsum fuga commodi aperiam similique, iusto maiores animi reiciendis recusandae ad quo illo aliquam maxime error facere aut doloribus, pariatur id aliquid repellat labore! Assumenda fuga nam maxime rem repellat reprehenderit, numquam, ut cumque harum facilis laboriosam id optio illum. Ab molestiae, delectus sint cumque facere excepturi vero consequuntur dolorum quam labore placeat, hic aliquam. Voluptatibus, possimus? Expedita magni maiores veniam, soluta, quo earum beatae modi temporibus reiciendis ratione, similique sed? Nisi, maxime. Fuga aliquam nobis sit, ad, provident quisquam aspernatur rerum iste eveniet sequi ex delectus, suscipit beatae earum?"
-                isVisible={sectionConfig.aboutInstaMart}
-                SetIsVisible={() => setSectionConfig({
-                    aboutInstaMart: true,
-                    teamInstaMart: false,
-                    careerInstaMart: false,
-                })
-
-                }
+                isVisible={visibleSection == "about"}
+                SetIsVisible={(visible) => visible ? setIsVisibleSection('about') : setIsVisibleSection("")}
 
             />
             <Section
                 title="team InstaMart"
                 paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti atque ipsum fuga commodi aperiam similique, iusto maiores animi reiciendis recusandae ad quo illo aliquam maxime error facere aut doloribus, pariatur id aliquid repellat labore! Assumenda fuga nam maxime rem repellat reprehenderit, numquam, ut cumque harum facilis laboriosam id optio illum. Ab molestiae, delectus sint cumque facere excepturi vero consequuntur dolorum quam labore placeat, hic aliquam. Voluptatibus, possimus? Expedita magni maiores veniam, soluta, quo earum beatae modi temporibus reiciendis ratione, similique sed? Nisi, maxime. Fuga aliquam nobis sit, ad, provident quisquam aspernatur rerum iste eveniet sequi ex delectus, suscipit beatae earum?"
-                isVisible={sectionConfig.teamInstaMart}
-                SetIsVisible={() => setSectionConfig({
-                    aboutInstaMart: false,
-                    teamInstaMart: true,
-                    careerInstaMart: false,
-                })
-
-                }
+                isVisible={visibleSection == "team"}
+                SetIsVisible={(visible) => visible ? setIsVisibleSection('team') : setIsVisibleSection("")}
 
             />
             <Section
                 title="Career InstaMart"
                 paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti atque ipsum fuga commodi aperiam similique, iusto maiores animi reiciendis recusandae ad quo illo aliquam maxime error facere aut doloribus, pariatur id aliquid repellat labore! Assumenda fuga nam maxime rem repellat reprehenderit, numquam, ut cumque harum facilis laboriosam id optio illum. Ab molestiae, delectus sint cumque facere excepturi vero consequuntur dolorum quam labore placeat, hic aliquam. Voluptatibus, possimus? Expedita magni maiores veniam, soluta, quo earum beatae modi temporibus reiciendis ratione, similique sed? Nisi, maxime. Fuga aliquam nobis sit, ad, provident quisquam aspernatur rerum iste eveniet sequi ex delectus, suscipit beatae earum?"
-                isVisible={sectionConfig.careerInstaMart}
-                SetIsVisible={() => setSectionConfig({
-                    aboutInstaMart: false,
-                    teamInstaMart: false,
-                    careerInstaMart: true,
-                })
-
-                }
+                isVisible={visibleSection == "career"}
+                SetIsVisible={(visible) => visible ? setIsVisibleSection('career') : setIsVisibleSection("")}
 
             />
         </div>
